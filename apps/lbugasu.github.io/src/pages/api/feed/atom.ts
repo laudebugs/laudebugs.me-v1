f77b51e19ae37b0cb71eb6798c4fc61d67f5b0ea
Laurence Ininda <lbugasu@gmail.com>
2021-11-16 08:26:53 -0600

    fix(title): title missing in dev and journal

--- apps/lbugasu.github.io/src/pages/api/feed/atom.ts
+++ apps/lbugasu.github.io/src/pages/api/feed/atom.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 
-import feed from '../../../../../../posts/feed.json'
 import devFeed from '../../../../../../posts/dev.json'
+import feed from '../../../../../../posts/feed.json'
 import journalFeed from '../../../../../../posts/journal.json'
 
 const handler = nc<NextApiRequest, NextApiResponse>()
@@ -12,7 +12,9 @@ handler.get((request, response) => {
     response.send(devFeed.atom)
   } else if (request.query?.section === 'journal') {
     response.send(journalFeed.atom)
-  } else response.send(feed.atom)
+  } else {
+    response.send(feed.atom)
+  }
 })
 
 export default handler
