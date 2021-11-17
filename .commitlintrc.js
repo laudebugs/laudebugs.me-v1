c10d7a6cfac6542749e4d2e0797517a02246c165
lbugasu <lbugasu@gmail.com>
2021-11-16 20:53:49 -0600

    :bulb: article(article): :bulb: article(article): organizing Codebases
    A guide on organizing codebases using commitling, commitizen, eslint, prettier, versioning
    and
    changelog generation

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['gitmoji'] }
+module.exports = {
+  extends: ['gitmoji'],
+  rules: {
+    'type-enum': [2, 'always', ['article', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']]
+  }
+}
