f42f70a9ca768bd16ab092ee61f60392fb9b58e6
Laurence Ininda <laudebugs@laudebugs-108.lan>
2021-12-05 16:58:34 -0600

    :construction: wip(blog): remove posts folder

--- posts/feed.js
+++
@@ -1,9 +0,0 @@
-const { getFilesFromDir, writeFeed } = require('./helpers/helpers')
-
-const devPostData = getFilesFromDir('dev', true)
-const journalPostData = getFilesFromDir('journal', true)
-const combinedData = [...devPostData, ...journalPostData]
-
-writeFeed(devPostData, 'dev')
-writeFeed(journalPostData, 'journal')
-writeFeed(combinedData, 'feed')
