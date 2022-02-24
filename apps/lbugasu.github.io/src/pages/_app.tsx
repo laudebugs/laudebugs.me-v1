fe1edb2e18187ed09e05830d2a582dc3ac5c18cf
lbugasu <lbugasu@gmail.com>
2022-02-24 14:37:30 -0600

    feat: npm + yarn scripts

--- apps/lbugasu.github.io/src/pages/_app.tsx
+++ apps/lbugasu.github.io/src/pages/_app.tsx
@@ -6,10 +6,15 @@ import { useEffect, useState } from 'react'
 import { fromEvent } from 'rxjs'
 import { distinctUntilChanged, map } from 'rxjs/operators'
 import { Progress, ThemeProvider } from 'theme-ui'
+import { Provider } from 'react-redux'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import theme from '../styles/theme'
 import './styles.scss'
+import { store } from '@sandstorm/redux'
+import { useIsBrowser } from '@sandstorm/hooks/useIsBrowser'
+import { getLocalStorageItem } from '@sandstorm/helpers/localStorage'
+import { appActions } from '@sandstorm/redux/store'
 
 const components = {
   // eslint-disable-next-line react/display-name
@@ -23,6 +28,14 @@ function CustomApp({ Component, pageProps }: AppProps) {
   function calculateScrollPercent(scrollTop, scrollHeight, clientHeight) {
     return Math.floor((scrollTop / (scrollHeight - clientHeight)) * 10000) / 10000
   }
+
+  const isBrowser = useIsBrowser()
+  useEffect(() => {
+    if (isBrowser) {
+      appActions.setIsNpm((getLocalStorageItem<boolean>('isNpm') || !getLocalStorageItem<boolean>('isYarn')) ?? false)
+    }
+  }, [isBrowser])
+
   useEffect(() => {
     const scroll$ = fromEvent(document, 'scroll')
     const progress$ = scroll$.pipe(
@@ -42,26 +55,28 @@ function CustomApp({ Component, pageProps }: AppProps) {
   }, [])
 
   return (
-    <ThemeProvider theme={theme} components={components}>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
-      <div className="contentBody">
-        <Progress sx={{ variant: 'containers.progressBar' }} className="progressBar" max={1} value={progress}>
-          {progress * 100}%
-        </Progress>
-        <span className="content">
-          <Header />
-          <Component {...pageProps} />
+    <Provider store={store}>
+      <ThemeProvider theme={theme} components={components}>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+        </Head>
+        <div className="contentBody">
+          <Progress sx={{ variant: 'containers.progressBar' }} className="progressBar" max={1} value={progress}>
+            {progress * 100}%
+          </Progress>
+          <span className="content">
+            <Header />
+            <Component {...pageProps} />
+          </span>
+        </div>
+        <span className="footer">
+          <Footer />
         </span>
-      </div>
-      <span className="footer">
-        <Footer />
-      </span>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
